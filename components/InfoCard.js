829a81ac323fb79d422652a9e390f82c6440a88a
michaelraysor <michaelraysor1@gmail.com>
2021-08-30 21:28:52 -0400

    Updated shadow and CSS

--- components/InfoCard.js
+++ components/InfoCard.js
@@ -4,16 +4,38 @@ import { StarIcon } from "heroicons/react/solid";
 
 function InfoCard({ img, location, title, description, star, price, 
     total }) {
+
     return (
-        <div>
+        <div className= "flex">
         <div className="relative h-24 w-40 md:h-52 md:w-80 flex-shrink-0">
           <Image src={img} layout="fill" objectFit="cover" />
           </div>
-        
-        <div>
-        <p> { location } </p>
 
+        <div className = "flex flex-col flex-grow pl-5">
+        <div className="flex justify-between">
+        <p> { location } </p>
+          <HeartIcon className ="h-7 cursor-pointer" />
         </div>
+
+        <h4 className="text-xl"> {title} </h4>
+
+        <div className="border-b w-10 pt-2" />
+        <p className="pt-2 text-sm text-gray-500 flex-grow">{description}</p>
+        <div>
+        <p className="flex items-center">
+         <StarIcon className=" h-5
+          text-red-400"/>
+          {star}
+        </p>
+        <p>
+        {price}
+        </p>
+        <p>
+        {total}
+        </p>
+    
+        </div>
+    </div>
     </div>
         
     );
