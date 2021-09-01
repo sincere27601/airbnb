1018bb4bde638861ec436eaf09b0f1ce5fab66da
michaelraysor <michaelraysor1@gmail.com>
2021-08-31 21:23:44 -0400

    Updated Search

--- components/Header.js
+++ components/Header.js
@@ -58,10 +58,10 @@ function Header({placeholder}) {
             <div className="flex items-center md:border-2 
             rounded-full py-2 md:shadow-sm">
 
-            <input value= {searchInput} onChange={ (e) => setSearchInput(e.target.value)} 
+            <input value= {searchInput} onChange={(e) => setSearchInput(e.target.value)} 
             className=" flex-grow pl-5 bg-transparent outline:none test-sm text-size-gray-600"  
             type="text"
-            placeholder={placeholder || "Start your search"}/>
+            placeholder={placeholder || "Start your Vacation"}/>
 
             <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2"/>
             </div>
