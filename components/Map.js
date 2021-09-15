4d7c9abad08210051429e7af065a8d3d12c86532
michaelraysor <michaelraysor1@gmail.com>
2021-09-15 06:10:47 -0400

    Viewport

--- components/Map.js
+++ components/Map.js
@@ -1,8 +1,16 @@
+import { useState } from "react";
 import ReactMapGL from 'react-map-gl';
 
 function Map() {
+
+    const [viewport, setViewort] = useState({
+width: '100%',
+height: '100%',
+    });
     return (
-        <ReactMapGL mapStyle='mapbox://styles/sincere27601/cktlb2utc0bbt18onqlv684rg'>
+        <ReactMapGL mapStyle='mapbox://styles/sincere27601/cktlb2utc0bbt18onqlv684rg' mapboxApiAccessToken={process.env.mapbox_key}
+        
+        >
         
         </ReactMapGL>
     )
--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     images: {
         domains: [ 'links.papareact.com'],
-    }
+    },
     env: {
         mapbox_key: 'pk.eyJ1Ijoic2luY2VyZTI3NjAxIiwiYSI6ImNrdGxheXFnbjE0N2IydG1zNDUyeHJrcDIifQ.ES7pprj8LpGk758-kAeIfw',
     }
