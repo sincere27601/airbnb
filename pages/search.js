875a52a9acd4decfe2d008bb005a187225247f0b
michaelraysor <michaelraysor1@gmail.com>
2021-09-15 16:32:09 -0400

    Updated central map

--- pages/search.js
+++ pages/search.js
@@ -3,6 +3,7 @@ import Footer from "../components/Footer";
 import { useRouter } from "next/dist/client/router";
 import { format } from "date-fns";
 import InfoCard from "../components/InfoCard";
+import Map from "../components/Map";
 
 function Search({ searchResults }) {
 
